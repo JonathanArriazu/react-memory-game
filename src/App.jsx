29aea8e1c49b983e23c3101e3d0ffd67219a4df1
Jonathan Arriazu <joni.arriazu2@gmail.com>
2024-01-25 15:22:03 -0300

    feat: button added to change background

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Cards from './components/Cards';
 import Timer from './components/Timer';
+import './index.css';
 
 function App() {
 
   const [start, setStart] = useState(false);
+  const [background, setBackground] = useState('/images/background1.jpg');
+
+  const changeBackground = () => {
+    if(!start){
+    setBackground((prevBackground) =>
+      prevBackground === '/images/background1.jpg'
+        ? '/images/background2.jpg'
+        : '/images/background1.jpg'
+    );
+    }
+  };
+
+  useEffect(() => {
+    if(!start) {
+      document.body.style.backgroundImage = `url('${background}')`;
+    }
+  }, [background]);
 
   return (
     <div className="App">
       <h1>Memory Game</h1>
+      <button onClick={changeBackground}>Cambiar fondo</button>
       <Timer start={start} setStart={setStart}/>
       <div className='buttons'>
         {start ? 
